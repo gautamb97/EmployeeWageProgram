5def00969fb4041e9f936286461aa337fd0c5123
gautamb97 <www.gautam971997@gamail.com>
2021-02-11 09:34:30 +0530

    Added:Part_Time_Employee_and_Wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -6,16 +6,19 @@ public class EmployeeWageBuilder {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
 		 //Constants
-	    int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 1;
+	    int IS_FULL_TIME = 2;
 	    int EMP_RATE_PER_HOUR = 20;
         //Variables 
         int empHrs = 0;
         int empWage = 0;
         
         //Computation
-        double employeeCheck = Math.floor(Math.random() * 10) % 2;
+        double employeeCheck = Math.floor(Math.random() * 10) % 3;
         if(employeeCheck == IS_FULL_TIME)
             empHrs = 8;
+        else if (employeeCheck == IS_PART_TIME)
+        	empHrs = 4;
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage: " + empWage);
 
